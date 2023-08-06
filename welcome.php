7a43bf79f2460f2bb379924c7f73715944c116dc
Nimsha V S <nimshashajahan@gmail.com>
2023-08-07 01:23:49 +0530

    first commit

---
+++ welcome.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your email address is: <?php echo $_POST["email"]; ?>
+<?php
+echo "<h2>Your Input:</h2>";
+echo $name;
+echo "<br>";
+echo $email;
+echo "<br>";
+echo $comment;
+echo "<br>";
+echo $gender;
+?>
+</body>
+</html>
